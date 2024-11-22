55e8db7bf8920919472d8c34fbac8cb9af58d28e
os_pro <siboruremaobed90@gmsil.com>
2024-11-22 16:59:30 +0200

    first commit

---
+++ about.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SHINE BAKERY</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<style type="text/css">
+		*
+	{
+		font-family: arial;
+		margin: 0px;
+	}
+
+</style>
+<body>
+	<!-- Footer division -->
+	<div class="a">
+		<ol style="color: white;padding: 5px;">
+			<li>| (782)824-186|</li>
+			<li>support@shinebakery.com</li>
+			<li class="h">We are open:Mon-Sat 9.00-16.00</li>
+		</ol>
+	</div>
+	<!-- Navigation bar division -->
+	<div class="navbar">
+		<ul>
+			
+			<li><a href="products.html">PRODUCTS</a></li>
+			<li><a href="contact.html">CONTACT</a></li>
+			<li class="hn"><a href="#" style="color: white;font-style: bold;font-size: 17px;">|| ABOUT</a></li>
+			<li class=""><a href="index.html"> HOME</a></li>
+			<li class="hn"><h3>SHINE BAKERY</h3></li>
+		</ul>
+	</div>
+	<!-- About us bg image -->
+	<div class="col1-about">
+
+	</div>
+	<!-- About us -->
+	<div class="ftr" style="float: right;margin-top: -500px;width: 55%;height: 400px;font-size: 17px;">
+<h2 style="text-align: center;">About us</h2><br>
+<p>We are a gourmet cupcake bakery and creamery, and we bake and frost 18 signature</p>
+<p> flavors every morning, fresh from scratch. Our menu features seasonal and unique </p>
+<p>flavors created by Jeff Martin, the founder of Smallcakes.</p>
+
+<p>Our Story: Smallcakes Fort Myers is owned and operated by Don and Tonyia Moyer, </p>
+<p>a local couple, and is dedicated to bringing you the most memorable cupcake and creamery</p> 
+<p>experience. We are located in the beautiful Gulf Coast town of Fort Myers and have been</p> 
+<p>serving the community since 2013.</p>
+
+<p>Featured on TV: Smallcakes has proven to be a nationwide success. Our cupcake bakery has</p> 
+<p>been featured for three seasons running on the Food Network's "CUPCAKE WARS", an appearance</p>
+ <p>on ABC'S hit daytime talk show, "THE VIEW", and was selected as a "Must Try Cupcake Shop"</p>
+ <p> by USA TODAY. We are honored to have the opportunity to bring gourmet cupcakes to the </p>
+ <p> Southwest Florida community.</p>
+
+<p>Made Fresh, With Care: We make our gourmet cupcakes on-site every day using only the highest</p>
+ <p>quality ingredients, and we sweeten them with love! We also provide personalized cakes for</p> 
+ <p>weddings, birthdays, baby showers, business gatherings, and any other special occasion.</p>
+ <p> With a delectable cake that looks as good as it tastes, we aim to make your event one to remember</p>
+
+<p>What We Offer: We offer a variety of gourmet cupcakes, custom cakes, cheesecake, and other</p>
+<p> sweet treats. Our menu is updated seasonally, so there's always something new to try.</p> 
+<p> We also offer gluten-free and vegan options, so everyone can enjoy our delicious cupcakes.</p>
+	</div>
+	<!-- Footer -->
+		<div style="background: #111111;color: white;width: 100%;height: 39px;text-align: center;position: absolute;margin-bottom: 420px;">
+		&copy;2024  <b style="color: seagreen;cursor: pointer;">SHINE BAKERY</b> || All right reserved
+	</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,50 @@
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes" >
+  <!-- Site Metas -->
+  <meta name="keywords" content="" >
+  <meta name="description" content="" >
+  <meta name="author" content="" >
+    <title>ONLINE MIXED MARKETING SYSTEM</title>
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+</head>
+<body>
+    <header>
+    <div>
+        <h2>
+        <li>We are open: Mon-Sun 06:00am-22:00pm</li>
+        <li>|| support@mixedmarketing.com</li>
+        <li>|| +250780846856</li>
+        </h2>
+    </div>
+    </header>
+    <nav><br>
+        <strong><li><a href="">||MARKETING</a></li></strong>
+        <ul>
+        <li><a href="">HOME</a></li>
+        <li><a href="product.php">PRODUCT</a></li>
+        <li><a href="">CONTACT</a></li>
+        <li><a href="">ABOUT</a></li>
+        <li><a href="login.php">LOGIN</a></li>
+        <li><a href="signup.php">SIGN UP</a></li>
+    </ul>
+    </nav>
+    <main>
+    <div class="wlc"><br>
+        <h1><b>WELCOME 
+            TO OUR FISH FARMING <br>PLATFORM</b></h1>
+    </div>
+    </main>
+    <footer><br>
+        <strong>COPYRIGHT &copy; <?php
+         $t=date("m");
+         echo date('Y'); ?> MIXED MARKETING
+         </strong>
+    </footer>
+</body>
+</html>
